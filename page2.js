fe644f05162a238583f9249a52e5543518935f67
tuvalisle <151667091+tuvalisle@users.noreply.github.com>
2024-10-04 12:34:52 +0200

    Update page2.js

--- page2.js
+++ page2.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Function to fetch posts 
     const fetchPosts = async () => {
         try {
-            const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=3`);
+            const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=6`);
             const posts = await response.json();
 
             posts.forEach(post => {
